5fab80298071fbd90fb2fff83eae61f3b7e41413
Antoinette Janus <acj.gfx@gmail.com>
2017-08-02 09:49:24 -0400

    No errors

--- src/containers/update-button.js
+++ src/containers/update-button.js
@@ -3,14 +3,13 @@ import { send } from 'redux-electron-ipc'
 import UpdateButtonPresenter from '../components/update-button-presenter.js'
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(state)
   return {
     btn_text: ownProps.btn_text ? ownProps.btn_text : ''
   }
 }
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    updateValue: (e, btn_text) => {
+    updateAmount: (e, btn_text) => {
       e.preventDefault()
       // dispatch(toggleForm(ownProps.expanded))
       // dispatch(send('add-to-do', { title: title, details: details, complete: false, id: id }))
